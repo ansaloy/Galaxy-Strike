96ae47ac84d2cdbf1d92b229ebe192fd7792aea5
PavelKurganskui <comp9@Mac-mini-comp9.local>
2014-11-25 16:45:49 +0200

    0.0.0.4
    added main menu and new game menu

---
+++ Galaxy Strike/Assets/Scripts/galaxysize.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class galaxysize : MonoBehaviour {
+
+	public UILabel percent;
+	public UIScrollBar scrollbar;
+
+	// Use this for initialization
+	public void OnUpdatePercentLabel () {
+		int pr = 10+(int)(90 * scrollbar.value);
+		percent.text = pr.ToString()+"%";
+		create.spaceLimit = 50+(int)(50*scrollbar.value);
+	}
+}
