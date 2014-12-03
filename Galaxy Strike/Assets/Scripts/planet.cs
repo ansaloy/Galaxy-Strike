23fd7b5c910224237f01371071ce3c0ed2d6d580
Salo Andrew <comp9@Mac-mini-comp9.local>
2014-12-03 16:40:38 +0200

    0.0.2.7

--- Galaxy Strike/Assets/Scripts/planet.cs
+++ Galaxy Strike/Assets/Scripts/planet.cs
@@ -35,6 +35,7 @@ public class planet : MonoBehaviour {
 
 		//зминюємо колbр кільця навколо планети на колір гравця
 		if (owner != gamedata.planetsOwner [index]) {
+			transform.Find("Background").gameObject.SetActive(true);
 			owner = gamedata.planetsOwner [index];
 			transform.Find ("Background").GetComponent<SpriteRenderer> ().color = gamedata.playersColor [owner];
 		}
